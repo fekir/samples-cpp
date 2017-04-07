2568c64ddce04baece0ff08bca27b72e09a22bb5
fekir <federico.kircheis@gmail.com>
2017-04-07 18:53:01 +0200

    added stringstream for comparing results of test case

--- streams/test.cpp
+++ streams/test.cpp
@@ -6,15 +6,21 @@ TEST_CASE("string_view", "[string_view]"){
     const char data[] = "123 c 456";
     const auto len = 7;
     streambufview strv(&data[1], len); // no copy!, and '\0'-termination not required
-    std::istream iss(&strv);
+    std::istream isv(&strv);
+    std::stringbuf strb(std::string(&data[1], len));
+    std::istream iss(&strb); // should be an istringstream
     SECTION("read content"){
         int a{};
         int b{};
         char c{};
-        REQUIRE(iss >> a >> c >> b);
-        REQUIRE(a== 23);
-        REQUIRE(c=='c');
-        REQUIRE(b== 45);
+        REQUIRE(isv >> a >> c >> b);
+        int a2{};
+        int b2{};
+        char c2{};
+        REQUIRE(iss >> a2 >> c2 >> b2);
+        REQUIRE(a== a2);
+        REQUIRE(c==c2);
+        REQUIRE(b== b2);
     }
 
     SECTION("fail to read numbers"){
@@ -22,50 +28,84 @@ TEST_CASE("string_view", "[string_view]"){
         int b{};
         int c{};
         REQUIRE_FALSE(iss >> a >> b >> c);
+        REQUIRE_FALSE(isv >> a >> b >> c);
     }
+
     SECTION("in_avail"){
         auto i = strv.in_avail();
+        auto i2 = strb.in_avail();
         REQUIRE(i == len);
-        REQUIRE(iss >> i);
-        REQUIRE(i == 23);
+        REQUIRE(i == i2);
+        REQUIRE(isv >> i);
+        REQUIRE(iss >> i2);
+        REQUIRE(i == i2);
         i = strv.in_avail();
-        REQUIRE(i == 5);
+        i2 = strb.in_avail();
+        REQUIRE(i == i2);
         char c{};
-        REQUIRE(iss >> c);
-        REQUIRE(c == 'c');
+        char c2{};
+        REQUIRE(isv >> c);
+        REQUIRE(iss >> c2);
+        REQUIRE(c == c2);
         i = strv.in_avail();
-        REQUIRE(i == 3);
-        REQUIRE(iss >> i);
-        REQUIRE(i == 45);
+        i2 = strb.in_avail();
+        REQUIRE(i == i2);
+        REQUIRE(isv >> i);
+        REQUIRE(iss >> i2);
+        REQUIRE(i == i2);
         i = strv.in_avail();
+        i2 = strb.in_avail();
         REQUIRE(i == 0);
+        REQUIRE(i == i2);
     }
     SECTION("snextc/sgetc/sgetn"){
-        auto c = strv.sgetc();
-        REQUIRE(c == '2');
-        c = strv.snextc();
-        REQUIRE(c == '3');
-        c = strv.sgetc();
-        REQUIRE(c == '3');
-        c = strv.snextc();
-        REQUIRE(c == ' ');
+        auto c1 = strv.sgetc();
+        auto c2 = strb.sgetc();
+        REQUIRE(c1 == '2');
+        REQUIRE(c1 == c2);
+        c1 = strv.snextc();
+        c2 = strb.snextc();
+        REQUIRE(c1 == '3');
+        REQUIRE(c1 == c2);
+        c1 = strv.sgetc();
+        c2 = strb.sgetc();
+        REQUIRE(c1 == '3');
+        c1 = strv.snextc();
+        c2 = strb.snextc();
+        REQUIRE(c1 == ' ');
+        REQUIRE(c1 == c2);
         char tmp[3]{};
         strv.sgetn(tmp, sizeof(tmp));
+        char tmp2[3]{};
+        strb.sgetn(tmp2, sizeof(tmp2));
         REQUIRE(tmp[0] == ' ');
+        REQUIRE(tmp[0] == tmp2[0]);
         REQUIRE(tmp[1] == 'c');
+        REQUIRE(tmp[1] == tmp2[1]);
         REQUIRE(tmp[2] == ' ');
+        REQUIRE(tmp[2] == tmp2[2]);
     }
     SECTION("sungetc"){
-        auto c = strv.sgetc();
-        REQUIRE(c == '2');
-        c = strv.sungetc();
-        REQUIRE(c == EOF);
-        REQUIRE(iss >> c);
-        REQUIRE(c==23);
-        c = strv.sgetc();
-        REQUIRE(c==' ');
-        c = strv.sungetc();
-        REQUIRE(c=='3');
+        auto c1 = strv.sgetc();
+        auto c2 = strb.sgetc();
+        REQUIRE(c1 == '2');
+        REQUIRE(c1 == c2);
+        c1 = strv.sungetc();
+        c2 = strb.sungetc();
+        REQUIRE(c1 == EOF);
+        REQUIRE(c1 == c2);
+        REQUIRE(isv >> c1);
+        REQUIRE(iss >> c2);
+        REQUIRE(c1==23);
+        REQUIRE(c1==c2);
+        c1 = strv.sgetc();
+        c2 = strb.sgetc();
+        REQUIRE(c1==' ');
+        REQUIRE(c1==c2);
+        c1 = strv.sungetc();
+        c2 = strb.sungetc();
+        REQUIRE(c1=='3');
+        REQUIRE(c1==c2);
     }
 }
 
