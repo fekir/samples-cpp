576b37454b17339b27acd1eba7b9a7b319f3aff3
fekir <federico.kircheis@gmail.com>
2017-04-08 16:05:15 +0200

    enhanced test case for EventReporter

--- streams/test.cpp
+++ streams/test.cpp
@@ -1,5 +1,17 @@
 #include <catch.hpp>
 
+#ifdef _WIN32
+
+
+#include <atomic>
+#include <condition_variable>
+#include <thread>
+#include <chrono>
+
+#include <windows.h>
+#include <winevt.h>
+#endif
+
 #include "streams.hpp"
 
 TEST_CASE("string_view", "[string_view][istream]"){
@@ -171,6 +183,116 @@ TEST_CASE("file", "[file][istream][ostream]"){
 
 
 #ifdef _WIN32
+struct EvtClose_functor {
+	typedef EVT_HANDLE pointer;
+	void operator()(const EVT_HANDLE h) const noexcept {
+		const auto res = ::EvtClose(h);
+		assert(res == TRUE); (void)res;
+	}
+};
+using unique_EVTHANDLE = std::unique_ptr<EVT_HANDLE, EvtClose_functor>;
+
+// utf8-encoded
+std::string ws2s(const std::wstring& s) {
+	if (s.empty()) {
+		return "";
+	}
+	if (s.length() > (static_cast<size_t>((std::numeric_limits<int>::max)()))) {
+		throw std::runtime_error("string is too long");
+	}
+	constexpr auto flag = WC_ERR_INVALID_CHARS; // alternative to WC_ERR_INVALID_CHARS is 0
+												// https://msdn.microsoft.com/en-us/library/windows/desktop/dd374130%28v=vs.85%29.aspx
+	int len = WideCharToMultiByte(CP_UTF8, flag, s.c_str(), static_cast<int>(s.length()), nullptr, 0, nullptr, nullptr);
+	if (len <= 0) {
+		throw std::runtime_error("error when converting to string");
+	}
+	std::string buf(static_cast<size_t>(len), '\0');
+	len = WideCharToMultiByte(CP_UTF8, flag, s.c_str(), static_cast<int>(s.length()), &buf[0], len, nullptr, nullptr);
+	if (len <= 0) {
+		throw std::runtime_error("error when converting to string");
+	}
+	buf.resize(static_cast<size_t>(len));
+	return buf;
+}
+
+size_t getRequiredSize_XML(const EVT_HANDLE hEvent) {
+	DWORD dwBufferUsed = 0;
+	if (EvtRender(nullptr, hEvent, EvtRenderEventXml, 0, nullptr, &dwBufferUsed, nullptr)) {
+		return 0;
+	}
+	DWORD status = GetLastError();
+	if (ERROR_INSUFFICIENT_BUFFER != status) {
+		throw std::runtime_error("unexpected error while querying size for message:" + std::to_string(status));
+	}
+	return dwBufferUsed / 2;
+}
+
+std::string get_rendered_content(const EVT_HANDLE hEvent)
+{
+	DWORD dwBufferUsed = 0;
+	std::wstring renderedContent(getRequiredSize_XML(hEvent), L'\0');
+
+	if (!EvtRender(nullptr, hEvent, EvtRenderEventXml, static_cast<DWORD>(renderedContent.size()) * sizeof(wchar_t), &renderedContent.at(0), &dwBufferUsed, nullptr)) {
+		const DWORD status = GetLastError(); assert(ERROR_INSUFFICIENT_BUFFER != status);
+		throw std::runtime_error("unexpected error while querying message:" + std::to_string(status));
+	}
+	std::string toreturn = ws2s(renderedContent);
+	toreturn.erase(toreturn.find_last_not_of('\0') + 1);
+	return toreturn;
+}
+
+struct context {
+	std::condition_variable cv;
+	std::mutex cv_m;
+	std::vector<std::string> messages;
+	std::size_t total_events = 1;
+};
+
+DWORD WINAPI callback(const EVT_SUBSCRIBE_NOTIFY_ACTION action, PVOID pContext, const EVT_HANDLE hEvent) try {
+	assert(pContext != nullptr);
+	auto& ctx = *reinterpret_cast<context*>(pContext);
+	if (action != EvtSubscribeActionDeliver) {
+		return 0;
+	}
+
+	const auto var = get_rendered_content(hEvent);
+	{
+		std::lock_guard<std::mutex> lk(ctx.cv_m);
+		ctx.messages.push_back(var);
+		if (ctx.messages.size() == ctx.total_events) {
+			ctx.cv.notify_all();
+		}
+	}
+	return ERROR_SUCCESS;
+}
+catch (...) {
+	return ERROR_INVALID_DATA;
+}
+
+unique_EVTHANDLE register_callback(context& ctx) {
+
+	EVT_HANDLE session{};
+	HANDLE signal_event{};
+	const auto path = L"Application";
+	const auto query = L"*[System/EventID=0]";
+	EVT_HANDLE bookmark{};
+
+	unique_EVTHANDLE h(EvtSubscribe(session, signal_event, path, query, bookmark, &ctx, callback, EvtSubscribeToFutureEvents));
+	REQUIRE(h);
+	return h;
+}
+
+
+std::size_t count_messages(const std::string& msg, const std::string& to_find) {
+	size_t nPos = msg.find(to_find, 0); // fist occurrence
+	size_t count = 0;
+	while (nPos != std::string::npos)
+	{
+		++count;
+		nPos = msg.find(to_find, nPos + 1);
+	}
+	return count;
+}
 
 // buffer that contains PTOKEN_USER, accessible throug reinterpret_cast, in case of error empty buffer
 std::vector<unsigned char> get_token_user() {
@@ -189,7 +311,7 @@ std::vector<unsigned char> get_token_user() {
 	}
 	assert(dwBufferSize >= sizeof(TOKEN_USER));
 	buffer.resize(dwBufferSize);
-	auto pTokenUser = reinterpret_cast<PTOKEN_USER>(buffer.data());
+	const auto pTokenUser = reinterpret_cast<PTOKEN_USER>(buffer.data());
 
 	if (!GetTokenInformation(hToken, TokenUser, pTokenUser, dwBufferSize, &dwBufferSize))
 	{
@@ -211,22 +333,60 @@ TEST_CASE("eventsource", "[eventsource][ostream]") {
 
 	eventsource_buffer t("test", token_user->User.Sid);
 	std::ostream os(&t);
+
 	SECTION("single line") {
-		os << "hello world [single line]" << std::endl;
+		const std::string message = "hello world [single line]";
+
+		context ctx;
+		ctx.total_events = 1;
+
+		{
+			auto h = register_callback(ctx);
+			std::unique_lock<std::mutex> lk(ctx.cv_m);
+			os << message << std::endl;
+			using namespace std::chrono_literals;
+			REQUIRE(ctx.cv.wait_for(lk, 100ms) == std::cv_status::no_timeout);
+		}
+		REQUIRE(ctx.total_events == 1);
+
+		size_t count = count_messages(ctx.messages.at(0), message);
+		REQUIRE(count == 1);
+
 	}
+
 	SECTION("multiple lines at once(1)") {
-		os << "hello world [multiple1] 1\n";
-		os << "hello world [multiple2] 2\n";
-		os << "hello world [multiple3] 3\n";
-		os << "hello world [multiple4] 4\n";
-		os << std::flush;
+		const std::string message1 = "hello world [multiple1] 1";
+		const std::string message2 = "hello world [multiple2] 2";
+		context ctx;
+
+		{
+			auto h = register_callback(ctx);
+			std::unique_lock<std::mutex> lk(ctx.cv_m);
+			os << message1 + "\n" << message2 + "\n" << std::flush;
+			using namespace std::chrono_literals;
+			REQUIRE(ctx.cv.wait_for(lk, 100ms) == std::cv_status::no_timeout);
+		}
+		REQUIRE(ctx.total_events == 1);
+
+		size_t count = count_messages(ctx.messages.at(0), message1) + count_messages(ctx.messages.at(0), message2);
+		REQUIRE(count == 2);
 	}
 	SECTION("multiple lines at once(2)") {
-		os << "hello world [multiple2] 1\n";
-		os << "hello world [multiple2] 2\n";
-		os << "hello world [multiple2] 3\n";
-		os << "hello world [multiple2] 4"; // !
-		os << std::flush;
+		const std::string message1 = "hello world [multiple1] 1";
+		const std::string message2 = "hello world [multiple2] 2";
+		context ctx;
+
+		{
+			auto h = register_callback(ctx);
+			std::unique_lock<std::mutex> lk(ctx.cv_m);
+			os << message1 + "\n" << message2 << std::flush;
+			using namespace std::chrono_literals;
+			REQUIRE(ctx.cv.wait_for(lk, 100ms) == std::cv_status::no_timeout);
+		}
+		REQUIRE(ctx.total_events == 1);
+
+		size_t count = count_messages(ctx.messages.at(0), message1) + count_messages(ctx.messages.at(0), message2);
+		REQUIRE(count == 2);
 	}
 }
 
